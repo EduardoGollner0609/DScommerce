45458fb09aab68e7ecb44cd91b0410a50e8312bc
EduGollner0609 <dudugollner@gmail.com>
2024-10-27 06:52:00 -0300

    Criando camada Service.!

---
+++ backend/src/main/java/com/eduardo/ecommerce/services/CategoryService.java
@@ -0,0 +1,36 @@
+package com.eduardo.ecommerce.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eduardo.ecommerce.entities.Category;
+import com.eduardo.ecommerce.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	// Create
+	public Category save(Category category) {
+		return 	repository.save(category);
+	}	
+	
+	// Read/Retrieve
+	public Category findById(Long id) {
+		return repository.getReferenceById(id);
+	}
+	
+	// Update
+	public Category update(Long id, Category category) {
+		Category obj = repository.getReferenceById(id);
+		obj.setName(category.getName());
+		return repository.save(obj);
+	}
+	
+	// Delete
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+}
