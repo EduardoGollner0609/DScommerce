c47bd90dc97190eabe6475d22438da1dd65db65b
EduGollner0609 <dudugollner@gmail.com>
2024-10-28 05:41:51 -0300

    Adicionando OrderItemPk e Mapeando com JPA o OrderItem.

--- backend/src/main/java/com/eduardo/ecommerce/entities/OrderItem.java
+++ backend/src/main/java/com/eduardo/ecommerce/entities/OrderItem.java
@@ -1,16 +1,44 @@
 package com.eduardo.ecommerce.entities;
 
+import com.eduardo.ecommerce.entities.pk.OrderItemPk;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order_item")
 public class OrderItem {
 
+	@EmbeddedId
+	private OrderItemPk id = new OrderItemPk();
 	private Integer quantity;
 	private Double price;
 
 	public OrderItem() {
 	}
 
-	public OrderItem(Integer quantity, Double price) {
+	public OrderItem(Integer quantity, Double price, Product product, Order order) {
+		id.setOrder(order);
+		id.setProduct(product);
 		this.quantity = quantity;
 		this.price = price;
+	}
+
+	public Product getProduct() {
+		return id.getProduct();
+	}
+
+	public void setProduct(Product product) {
+		this.id.setProduct(product);
+	}
+
+	public Order getOrder() {
+		return id.getOrder();
+	}
+
+	public void setOrder(Order order) {
+		this.id.setOrder(order);
 	}
 
 	public Integer getQuantity() {
