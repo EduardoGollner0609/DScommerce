7c1cf4d7ac8f3ffcca2ed1e0b283556af97cca3b
EduGollner0609 <dudugollner@gmail.com>
2024-10-28 05:46:06 -0300

    Adicionando relacação da parte do Product e OrderItem.

--- backend/src/main/java/com/eduardo/ecommerce/entities/Product.java
+++ backend/src/main/java/com/eduardo/ecommerce/entities/Product.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,10 +24,13 @@ public class Product {
 	private Double price;
 	private String description;
 	private String imgUrl;
-	
+
 	@ManyToMany
-	@JoinTable(name="tb_product_category", joinColumns = @JoinColumn(name="product_id"), inverseJoinColumns = @JoinColumn(name="category_id"))
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
+
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
 
 	public Product() {
 	}
@@ -82,7 +86,11 @@ public class Product {
 	public Set<Category> getCategories() {
 		return categories;
 	}
+
+	public Set<OrderItem> getItems() {
+		return items;
+	}
 	
-	
+
 
 }
