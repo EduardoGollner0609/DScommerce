c0d14738a8a8086cfc5483a31d74ef2153df134f
EduGollner0609 <dudugollner@gmail.com>
2024-10-28 05:47:24 -0300

    Criando método para retornar pedidos de um produto.

--- backend/src/main/java/com/eduardo/ecommerce/entities/Product.java
+++ backend/src/main/java/com/eduardo/ecommerce/entities/Product.java
@@ -1,6 +1,7 @@
 package com.eduardo.ecommerce.entities;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
@@ -91,6 +92,8 @@ public class Product {
 		return items;
 	}
 	
-
+	public List<Order> getOrders() {
+		return items.stream().map(x -> x.getOrder()).toList();
+	}
 
 }
