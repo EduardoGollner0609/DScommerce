b5b6b309615c16b7bbcda6a4eff149dafc8ce905
EduGollner0609 <dudugollner@gmail.com>
2024-10-29 11:59:06 -0300

    Refatorando UserService.

--- backend/src/main/java/com/eduardo/ecommerce/services/UserService.java
+++ backend/src/main/java/com/eduardo/ecommerce/services/UserService.java
@@ -1,16 +1,13 @@
 package com.eduardo.ecommerce.services;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.eduardo.ecommerce.entities.Role;
 import com.eduardo.ecommerce.entities.User;
-import com.eduardo.ecommerce.projections.UserDetailsProjection;
 import com.eduardo.ecommerce.repositories.UserRepository;
 
 @Service
@@ -21,21 +18,20 @@ public class UserService implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
-		
-		if (result.size() == 0) {
-			throw new UsernameNotFoundException("");
+		User user = repository.findByEmail(username);
+		if (user == null) {
+			throw new UsernameNotFoundException("Email not found");
 		}
-
-		User user = new User();
-		user.setEmail(username);
-		user.setEmail(result.get(0).getPassword());
-
-		for (UserDetailsProjection projection : result) {
-			user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
-		}
-
 		return user;
 	}
 
+	protected User authenticated() {
+		try {
+			String username = SecurityContextHolder.getContext().getAuthentication().getName();
+			return repository.findByEmail(username);
+		}
+		catch (Exception e) {
+			throw new UsernameNotFoundException("Invalid user");
+		}
+	}
 }
