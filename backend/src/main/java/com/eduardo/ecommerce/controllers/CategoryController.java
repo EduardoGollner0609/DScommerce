a756a89ce3e57b1dd1feb2d04b6a582ce6690f98
EduGollner0609 <dudugollner@gmail.com>
2024-10-29 12:08:25 -0300

    Refatorando CategoryController.

--- backend/src/main/java/com/eduardo/ecommerce/controllers/CategoryController.java
+++ backend/src/main/java/com/eduardo/ecommerce/controllers/CategoryController.java
@@ -1,19 +1,12 @@
 package com.eduardo.ecommerce.controllers;
 
-import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.eduardo.ecommerce.dtos.CategoryDTO;
 import com.eduardo.ecommerce.services.CategoryService;
@@ -22,35 +15,12 @@ import com.eduardo.ecommerce.services.CategoryService;
 @RequestMapping(value = "/categories")
 public class CategoryController {
 
-	@Autowired
-	private CategoryService service;
+    @Autowired
+    private CategoryService service;
 
-	@PostMapping
-	public ResponseEntity<CategoryDTO> create(@RequestBody CategoryDTO dto) {
-		dto = service.save(dto);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
-		return ResponseEntity.created(uri).body(dto);
-	}
-
-	@GetMapping(value = "/{id}")
-	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
-		return ResponseEntity.ok(service.findById(id));
-	}
-
-	@GetMapping
-	public ResponseEntity<List<CategoryDTO>> findAll() {
-		return ResponseEntity.ok(service.findAll());
-	}
-
-	@PutMapping(value = "/{id}")
-	public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody CategoryDTO dto) {
-		dto = service.update(id, dto);
-		return ResponseEntity.ok(dto);
-	}
-
-	@DeleteMapping(value = "/{id}")
-	public ResponseEntity<Void> delete(@PathVariable long id) {
-		service.delete(id);
-		return ResponseEntity.noContent().build();
-	}
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<CategoryDTO> list = service.findAll();
+        return ResponseEntity.ok(list);
+    }
 }
