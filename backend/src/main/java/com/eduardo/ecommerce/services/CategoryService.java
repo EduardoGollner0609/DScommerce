f967e990261635be9429c521a90eb0777c865d5b
EduGollner0609 <dudugollner@gmail.com>
2024-10-29 12:07:18 -0300

    Refatorando CategoryService e adicionando anotação 'transactional'.

--- backend/src/main/java/com/eduardo/ecommerce/services/CategoryService.java
+++ backend/src/main/java/com/eduardo/ecommerce/services/CategoryService.java
@@ -4,13 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.eduardo.ecommerce.dtos.CategoryDTO;
 import com.eduardo.ecommerce.entities.Category;
 import com.eduardo.ecommerce.repositories.CategoryRepository;
-import com.eduardo.ecommerce.services.exceptions.ResourceNotFoundException;
-
-import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -18,48 +16,9 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 
-	// Create
-	public CategoryDTO save(CategoryDTO dto) {
-		Category category = new Category();
-		copyDtoToEntity(dto, category);
-		category = repository.save(category);
-		return new CategoryDTO(category);
-	}
-
-	// Read/Retrieve
-	public CategoryDTO findById(Long id) {
-		Category category = repository.findById(id)
-				.orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado."));
-		return new CategoryDTO(category);
-	}
-
+	@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll() {
-		List<Category> categories = repository.findAll();
-		return categories.stream().map(x -> new CategoryDTO(x)).toList();
-	}
-
-	// Update
-	public CategoryDTO update(Long id, CategoryDTO dto) {
-
-		try {
-			Category category = repository.getReferenceById(id);
-			copyDtoToEntity(dto, category);
-			category = repository.save(category);
-			return new CategoryDTO(category);
-		} catch (EntityNotFoundException e) {
-			throw new ResourceNotFoundException("Recurso não encontrado.");
-		}
-	}
-
-	// Delete
-	public void delete(Long id) {
-		if (!repository.existsById(id)) {
-			throw new ResourceNotFoundException("Recurso não encontrado.");
-		}
-		repository.deleteById(id);
-	}
-
-	private void copyDtoToEntity(CategoryDTO dto, Category category) {
-		category.setName(dto.getName());
+		List<Category> result = repository.findAll();
+		return result.stream().map(x -> new CategoryDTO(x)).toList();
 	}
 }
