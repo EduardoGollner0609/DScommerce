f201b473f8d6dce43ac01eaf8057016f1333adbf
EduGollner0609 <dudugollner@gmail.com>
2024-11-06 13:05:20 -0300

    Criando componente ProductDetailsCard e adicionando estilos/css.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
 import "./App.css";
 import HeaderClient from "./components/HeaderClient";
+import ProductDetailsCard from "./components/ProductDetailsCard";
 
 function App() {
   return (
     <>
       <HeaderClient />
+      <section id="product-details-section" className="dsc-container">
+        <ProductDetailsCard />
+        <div className="dsc-btn-page-container">
+          <div className="dsc-btn dsc-btn-blue">Comprar</div>
+          <div className="dsc-btn dsc-btn-white">Início</div>
+        </div>
+      </section>
     </>
   );
 }
