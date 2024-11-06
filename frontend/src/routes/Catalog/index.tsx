8294df9b3f351bb900007c9783d484b368a7bd05
EduGollner0609 <dudugollner@gmail.com>
2024-11-06 15:21:15 -0300

    Passando Product como Props no Catalog.

--- frontend/src/routes/Catalog/index.tsx
+++ frontend/src/routes/Catalog/index.tsx
@@ -1,8 +1,32 @@
 import HeaderClient from "../../components/HeaderClient";
 import "./styles.css";
 import SearchBar from "../../components/SearchBar";
-import CatalogCard from "../../components/CatalogCard";
 import ButtonNextPage from "../../components/ButtonNextPage";
+import { ProductDTO } from "../../models/Product";
+import CatalogCard from "../../components/CatalogCard";
+
+const product: ProductDTO = {
+  id: 2,
+  name: "SmartTv",
+  description: "Esta TV é maravilhosa.",
+  imgUrl:
+    "https://i5.walmartimages.com/asr/2aa59abd-3809-4cf5-9ebf-66365c38cb2e.1b3c37d9a592da238fd30c2100356ad7.jpeg",
+  price: 2500.99,
+  categories: [
+    {
+      id: 2,
+      name: "Eletronicos",
+    },
+    {
+      id: 3,
+      name: "Lindas",
+    },
+    {
+      id: 5,
+      name: "Fantasticas",
+    },
+  ],
+};
 
 export default function Catalog() {
   return (
@@ -13,18 +37,18 @@ export default function Catalog() {
           <SearchBar />
 
           <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
           </div>
 
           <ButtonNextPage />
