a89546ab4052b2502bea3bc91c8631d903c71386
EduGollner0609 <dudugollner@gmail.com>
2024-11-07 16:33:27 -0300

    Renderizando coleções dinamicamente.

--- frontend/src/routes/ClientHome/Catalog/index.tsx
+++ frontend/src/routes/ClientHome/Catalog/index.tsx
@@ -1,52 +1,18 @@
 import "./styles.css";
 import SearchBar from "../../../components/SearchBar";
 import ButtonNextPage from "../../../components/ButtonNextPage";
-import { ProductDTO } from "../../../models/Product";
 import CatalogCard from "../../../components/CatalogCard";
-
-
-const product: ProductDTO = {
-  id: 2,
-  name: "SmartTv",
-  description: "Esta TV é maravilhosa.",
-  imgUrl:
-    "https://i5.walmartimages.com/asr/2aa59abd-3809-4cf5-9ebf-66365c38cb2e.1b3c37d9a592da238fd30c2100356ad7.jpeg",
-  price: 2500.99,
-  categories: [
-    {
-      id: 2,
-      name: "Eletronicos",
-    },
-    {
-      id: 3,
-      name: "Lindas",
-    },
-    {
-      id: 5,
-      name: "Fantasticas",
-    },
-  ],
-};
+import * as ProductService from "../../../services/product-service";
 
 export default function Catalog() {
   return (
     <main>
       <section id="catalog-section" className="dsc-container">
         <SearchBar />
-
         <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
+          {ProductService.findAll().map((product) => (
+            <CatalogCard key={product.id} product={product} />
+          ))}
         </div>
 
         <ButtonNextPage />
