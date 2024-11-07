b11a4ac78a8857a3c4f4cb6ed471d1e994e72910
EduGollner0609 <dudugollner@gmail.com>
2024-11-07 17:05:42 -0300

    Chamando produto detalhado pelo ID.

--- frontend/src/routes/ClientHome/ProductDetails/index.tsx
+++ frontend/src/routes/ClientHome/ProductDetails/index.tsx
@@ -1,36 +1,18 @@
+import { useParams } from "react-router-dom";
 import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
-import { ProductDTO } from "../../../models/Product";
+import * as productService from "../../../services/product-service";
 import "./styles.css";
 
-const product: ProductDTO = {
-  id: 2,
-  name: "SmartTv",
-  description: "Esta TV é maravilhosa.",
-  imgUrl:
-    "https://i5.walmartimages.com/asr/2aa59abd-3809-4cf5-9ebf-66365c38cb2e.1b3c37d9a592da238fd30c2100356ad7.jpeg",
-  price: 2500.99,
-  categories: [
-    {
-      id: 2,
-      name: "Eletronicos",
-    },
-    {
-      id: 3,
-      name: "Lindas",
-    },
-    {
-      id: 5,
-      name: "Fantasticas",
-    },
-  ],
-};
 export default function ProductDetails() {
+  const params = useParams();
+  const product = productService.findById(Number(params.productId));
   return (
     <main>
       <section id="product-details-section" className="dsc-container">
-        <ProductDetailsCard product={product} />
+        {product && <ProductDetailsCard product={product} />}
+
         <div className="dsc-btn-page-container">
           <ButtonPrimary text="Comprar" />
           <ButtonInverse text="Inicio" />
