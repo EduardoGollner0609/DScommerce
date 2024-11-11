0feb26135f452a7561814a2c3ebb60c97b5abfd0
EduGollner0609 <dudugollner@gmail.com>
2024-11-11 15:37:20 -0300

    Adicionando link para retornar ao catalogo.

--- frontend/src/routes/ClientHome/Cart/index.tsx
+++ frontend/src/routes/ClientHome/Cart/index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { OrderDTO } from "../../../models/Order";
 import * as cartService from "../../../services/cart-service";
 import "./styles.css";
+import { Link } from "react-router-dom";
 
 export default function Cart() {
   const [cart, setCart] = useState<OrderDTO>(cartService.getCart());
@@ -44,7 +45,9 @@ export default function Cart() {
 
         <div className="dsc-btn-page-container">
           <div className="dsc-btn dsc-btn-blue">Finalizar pedido</div>
-          <div className="dsc-btn dsc-btn-white">Continuar comprando</div>
+          <Link to="/">
+            <div className="dsc-btn dsc-btn-white">Continuar comprando</div>
+          </Link>
         </div>
       </section>
     </main>
