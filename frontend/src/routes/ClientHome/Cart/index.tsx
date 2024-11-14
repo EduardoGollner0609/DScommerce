a8f132dad71b30e4ab2047289d329c90a7683b86
EduGollner0609 <dudugollner@gmail.com>
2024-11-14 17:44:19 -0300

    Trazendo função para o cart.

--- frontend/src/routes/ClientHome/Cart/index.tsx
+++ frontend/src/routes/ClientHome/Cart/index.tsx
@@ -7,6 +7,11 @@ import { Link } from "react-router-dom";
 export default function Cart() {
   const [cart, setCart] = useState<OrderDTO>(cartService.getCart());
 
+  function handleIncreaseItem(productId: number) {
+    cartService.increaseItem(productId);
+    setCart(cartService.getCart());
+  }
+
   return (
     <main>
       <section id="cart-container-section" className="dsc-container">
@@ -28,7 +33,12 @@ export default function Cart() {
                     <div className="dsc-cart-item-quantity-container">
                       <div className="dsc-cart-item-quantity-btn">-</div>
                       <p>{item.quantity}</p>
-                      <div className="dsc-cart-item-quantity-btn">+</div>
+                      <div
+                        onClick={() => handleIncreaseItem(item.productId)}
+                        className="dsc-cart-item-quantity-btn"
+                      >
+                        +
+                      </div>
                     </div>
                   </div>
                 </div>
