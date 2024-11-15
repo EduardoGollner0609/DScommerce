13406eee8e74200d09270d0d9474e7121db8fcf9
EduGollner0609 <dudugollner@gmail.com>
2024-11-15 01:28:37 -0300

    Exibindo valor do contextCartCount.

--- frontend/src/components/CartIcon/index.tsx
+++ frontend/src/components/CartIcon/index.tsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
+import { useContext } from "react";
 import cartIcon from "../../assets/cart.svg";
-import * as cartService from "../../services/cart-service";
 import "./syles.css";
+import { ContextCartCount } from "../../utils/context-cart";
 
 export default function CartIcon() {
-  const [cart, setCart] = useState(cartService.getCart());
+  const { contextCartCount } = useContext(ContextCartCount);
 
   return (
     <>
       <img src={cartIcon} alt="Carrinho de compras" />
-      <div className="dsc-cart-count">{cart.items.length}</div>
+      <div className="dsc-cart-count">{contextCartCount}</div>
     </>
   );
 }
