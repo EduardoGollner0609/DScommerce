763981e5827b91cef1f8c0bd95bca83b9e5622c8
EduGollner0609 <dudugollner@gmail.com>
2024-11-15 00:48:22 -0300

    Criando variavel armazenando cor vermelha para numeros de itens do carrinho.

--- frontend/src/components/CartIcon/index.tsx
+++ frontend/src/components/CartIcon/index.tsx
@@ -6,6 +6,7 @@ export default function CartIcon() {
   return (
     <Link to="/cart">
       <img src={cartIcon} alt="Carrinho de compras" />
+      <div className="dsc-cart-count">2</div>
     </Link>
   );
 }
