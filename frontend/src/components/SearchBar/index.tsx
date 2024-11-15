4ca22491c721e248a3f6b7ab9191b5808cb33e4f
EduGollner0609 <dudugollner@gmail.com>
2024-11-15 00:08:26 -0300

    Criando método e implementando lógica para pesquisa.

--- frontend/src/components/SearchBar/index.tsx
+++ frontend/src/components/SearchBar/index.tsx
@@ -1,10 +1,31 @@
+import { useState } from "react";
 import "./styles.css";
 
-export default function SearchBar() {
+type Props = {
+  onSearch: Function;
+};
+
+export default function SearchBar({ onSearch }: Props) {
+  const [text, setText] = useState("");
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setText(event.target.value);
+  }
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    onSearch(text);
+  }
+
   return (
-    <form className="dsc-search-bar">
+    <form className="dsc-search-bar" onSubmit={handleSubmit}>
       <button type="submit">🔎︎</button>
-      <input type="text" placeholder="Nome do produto" />
+      <input
+        value={text}
+        type="text"
+        placeholder="Nome do produto"
+        onChange={handleChange}
+      />
       <button type="reset">🗙</button>
     </form>
   );
