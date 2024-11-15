f3277dc08ca6c763274c1cd0c5238ce0eb9bb208
EduGollner0609 <dudugollner@gmail.com>
2024-11-15 03:41:57 -0300

    Implementando funções de logout/sair, get/pegar e save/salvar token na camada Service.

--- frontend/src/services/auth-service.ts
+++ frontend/src/services/auth-service.ts
@@ -3,11 +3,12 @@ import { CredentialsDTO } from "../models/auth";
 import { CLIENT_ID, CLIENT_SECRET } from "../utils/system";
 import { AxiosRequestConfig } from "axios";
 import { requestBackend } from "../utils/requests";
+import * as accesTokenRepository from "../localstorage/access-token-repository";
 
 export function loginRequest(loginData: CredentialsDTO) {
   const headers = {
     "Content-Type": "application/x-www-form-urlencoded",
-    Authorization: "Basic " + window.btoa(CLIENT_ID + ":" + CLIENT_SECRET)
+    Authorization: "Basic " + window.btoa(CLIENT_ID + ":" + CLIENT_SECRET),
   };
 
   const requestBody = QueryString.stringify({
@@ -23,4 +24,16 @@ export function loginRequest(loginData: CredentialsDTO) {
   };
 
   return requestBackend(config);
+}
+
+export function logout() {
+  accesTokenRepository.remove();
+}
+
+export function saveAccessToken(token: string) {
+  accesTokenRepository.save(token);
+}
+
+export function getAccessToken() {
+  accesTokenRepository.get();
 }
