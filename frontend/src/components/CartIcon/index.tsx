1d8962e0811455dcdb1a8143bbc9b8deb5d4250d
EduGollner0609 <dudugollner@gmail.com>
2024-11-15 01:08:22 -0300

    Criando estado global do cartCount.

--- frontend/src/components/CartIcon/index.tsx
+++ frontend/src/components/CartIcon/index.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import cartIcon from "../../assets/cart.svg";
+import * as cartService from "../../services/cart-service";
 import "./syles.css";
 
 export default function CartIcon() {
+  const [cart, setCart] = useState(cartService.getCart());
+
   return (
     <>
       <img src={cartIcon} alt="Carrinho de compras" />
-      <div className="dsc-cart-count">22</div>
+      <div className="dsc-cart-count">{cart.items.length}</div>
     </>
   );
 }
