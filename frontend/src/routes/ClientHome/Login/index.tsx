5d441304d0d695438448313ff7789d16d8e01ea1
EduGollner0609 <dudugollner@gmail.com>
2024-11-16 03:45:23 -0300

    Implementando condição para que a engrenagem apareça (somente com o role_admin).

--- frontend/src/routes/ClientHome/Login/index.tsx
+++ frontend/src/routes/ClientHome/Login/index.tsx
@@ -1,10 +1,12 @@
 import "./styles.css";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { CredentialsDTO } from "../../../models/auth";
 import * as authService from "../../../services/auth-service";
 import { useNavigate } from "react-router-dom";
+import { ContextToken } from "../../../utils/context-token";
 
 export default function Login() {
+  const { setContextTokenPayload } = useContext(ContextToken);
   const navigate = useNavigate();
   const [formData, setFormData] = useState<CredentialsDTO>({
     username: "",
@@ -14,6 +16,7 @@ export default function Login() {
     event.preventDefault();
     authService.loginRequest(formData).then((response) => {
       authService.saveAccessToken(response.data.access_token);
+      setContextTokenPayload(authService.getAccessTokenPayload());
       navigate("/cart");
     });
   }
