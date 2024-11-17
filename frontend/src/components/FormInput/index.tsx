04b3035074c9348479e08ea984e1fa4106ae26fd
EduGollner0609 <dudugollner@gmail.com>
2024-11-17 02:27:11 -0300

    Criando componente FormInput.

---
+++ frontend/src/components/FormInput/index.tsx
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+export default function FormInput(props: any) {
+  const { validation, ...inputProps } = props;
+  return <input {...inputProps} />;
+}
