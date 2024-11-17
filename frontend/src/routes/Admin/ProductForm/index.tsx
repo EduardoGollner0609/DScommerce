5301dfcb057a02e4631ea69aee07b56f7dc54bb0
EduGollner0609 <dudugollner@gmail.com>
2024-11-17 03:33:22 -0300

    Criando função Validate no formulário.

--- frontend/src/routes/Admin/ProductForm/index.tsx
+++ frontend/src/routes/Admin/ProductForm/index.tsx
@@ -25,6 +25,10 @@ export default function ProductForm() {
       name: "price",
       type: "number",
       placeholder: "Preço",
+      validation: function (value: any) {
+        return Number(value) > 0;
+      },
+      message: "Informe um preço positivo",
     },
     imgUrl: {
       value: "",
--- frontend/src/utils/forms.ts
+++ frontend/src/utils/forms.ts
@@ -6,16 +6,34 @@ export function update(inputs: any, name: string, newValue: any) {
 
 export function toValue(inputs: any) {
   const data: any = {};
+
   for (var name in inputs) {
     data[name] = inputs[name].value;
   }
+
   return data;
 }
 
 export function updateAll(inputs: any, newValues: any) {
   const newInputs: any = {};
+
   for (var name in inputs) {
     newInputs[name] = { ...inputs[name], value: newValues[name] };
   }
+
   return newInputs;
+}
+
+export function validate(inputs: any, name: string) {
+  
+  if (!inputs[name].validation) {
+    return inputs;
+  }
+
+  const isInvalid = !inputs[name].validation(inputs[name].value);
+
+  return {
+    ...inputs,
+    [name]: { ...inputs[name], invalid: isInvalid.toString() },
+  };
 }
