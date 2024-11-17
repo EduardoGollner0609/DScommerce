aa2b630b5a0d82f1bce0e46e495061f44f24e6c1
EduGollner0609 <dudugollner@gmail.com>
2024-11-17 04:42:43 -0300

    Criando componente TextArea.

--- frontend/src/routes/Admin/ProductForm/index.tsx
+++ frontend/src/routes/Admin/ProductForm/index.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import FormInput from "../../../components/FormInput";
 import * as forms from "../../../utils/forms";
 import * as productService from "../../../services/product-service";
+import FormTextArea from "../../../components/FormTextArea";
 
 export default function ProductForm() {
   const params = useParams();
@@ -40,6 +41,17 @@ export default function ProductForm() {
       name: "imgUrl",
       type: "text",
       placeholder: "Imagem",
+    },
+    description: {
+      value: "",
+      id: "description",
+      name: "description",
+      type: "text",
+      placeholder: "Descrição",
+      validation: function (value: string) {
+        return /^.{10,}$/.test(value);
+      },
+      message: "A descrição deve ter pelo menos 10 caracteres.",
     },
   });
 
@@ -95,6 +107,17 @@ export default function ProductForm() {
                   onChange={handleInputChange}
                 />
                 <div className="dsc-form-error">{formData.imgUrl.message}</div>
+              </div>
+              <div>
+                <FormTextArea
+                  {...formData.description}
+                  className="dsc-form-control dsc-textarea"
+                  onTurnDirty={handleTurnDirty}
+                  onChange={handleInputChange}
+                />
+                <div className="dsc-form-error">
+                  {formData.description.message}
+                </div>
               </div>
             </div>
 
