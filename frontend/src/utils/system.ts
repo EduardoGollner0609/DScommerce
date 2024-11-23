205435e9ea1e29d4a0b2ac055df22fa3eabe807c
EduGollner0609 <dudugollner@gmail.com>
2024-11-23 15:51:04 -0300

    Variaveis de ambiente do front end.

--- frontend/src/utils/system.ts
+++ frontend/src/utils/system.ts
@@ -1,5 +1,9 @@
-export const BASE_URL = "http://localhost:8080";
-export const TOKEN_KEY = "com.eduardo.ecommerce/Token";
-export const CART_KEY = "com.eduardo.ecommerce/Cart";
-export const CLIENT_ID = "myclientid";
-export const CLIENT_SECRET = "myclientsecret";
+export const TOKEN_KEY = "com.eduardo.dscommerce/Token";
+export const CART_KEY = "com.eduardo.dscommerce/Cart";
+
+export const BASE_URL =
+  import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8080";
+
+export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID ?? "myclientid";
+export const CLIENT_SECRET =
+  import.meta.env.VITE_CLIENT_SECRET ?? "myclientsecret";
